5e595e911927181ae66d1eb341eadc3647f4e024
Mikolaj Michal Rachwalski <mrachwalski@ultra60.mat.umk.pl>
2025-01-07 12:07:29 +0100

    Commit 2: Dodano NWD

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int nwd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
 int main() {
     int a, b;
     cin >> a >> b;
     cout << "Liczby: " << a << ", " << b << endl;
+    cout << "NWD: " << nwd(a, b) << endl;
     return 0;
 }
 
